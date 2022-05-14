2bbaf4ecb8f092911776692ff3101621e4b1be1c
LJM12914 <LJM12914@outlook.com>
2022-05-14 15:34:28 +0800

    0.0.12

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,27 @@
 ﻿"use strict";
-console.log("a");
+(_ => {
+    var canvas = document.getElementById("canvas");
+    var vContent = canvas.getContext("2d");
+    (_ => {
+        suitCanvasToWindow();
+    })();
+    window.onresize = suitCanvasToWindow;
+    function suitCanvasToWindow() {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+    }
+    canvas.onpointerdown = e => {
+    };
+    canvas.onpointermove = e => {
+    };
+    canvas.onpointerup = e => {
+    };
+    canvas.onpointerenter = e => {
+    };
+    canvas.onpointerleave = e => {
+    };
+    canvas.oncontextmenu = e => {
+        e.preventDefault();
+    };
+})();
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-﻿console.log("a");
+﻿(_=>{
+    var canvas = document.getElementById("canvas")! as HTMLCanvasElement;
+    var vContent = canvas.getContext("2d")!;
+    (_=>{ //初始化程序
+        suitCanvasToWindow();
+    })();
+    window.onresize = suitCanvasToWindow;
+    function suitCanvasToWindow(){
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+    }
+    canvas.onpointerdown = e=>{
+
+    }
+    canvas.onpointermove = e=>{
+
+    }
+    canvas.onpointerup = e=>{
+
+    }
+    canvas.onpointerenter = e=>{
+
+    }
+    canvas.onpointerleave = e=>{
+        
+    }
+    canvas.oncontextmenu = e=>{
+        e.preventDefault();
+        //todo:显示菜单
+    }
+})();
